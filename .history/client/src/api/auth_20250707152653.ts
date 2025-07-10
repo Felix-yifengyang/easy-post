db7a08a4da1793550a74e0e69541807b0290c848
felix <felix.yifengyang@outlook.com>
2025-07-10 10:25:37 +0800

    feat: gitignore ./history & delete git/.histroy

--- .history/client/src/api/auth_20250707152653.ts
+++
@@ -1,9 +0,0 @@
-import request from '../utils/request'
-
-export interface LoginResponse {
-  access_token: string;
-}
-
-export const login = (data: { username: string; password: string }) => {
-  return request.post<LoginResponse>('/auth/login', data)
-}
--- .history/client/src/components/Layout/Header_20250605180644.tsx
+++
@@ -1,9 +0,0 @@
-import { Link } from 'react-router-dom'
-
-export default function Header() {
-  return (
-    <header>
-      <Link to="/login">登录</Link>
-    </header>
-  )
-}
