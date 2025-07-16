a562064e44b69bdd2533aac7539a93038959d962
felix <felix.yifengyang@outlook.com>
2025-07-16 15:38:55 +0800

    fix: 全局布局样式修改

--- client/src/components/Layout/index.tsx
+++ client/src/components/Layout/index.tsx
@@ -1,11 +1,14 @@
 import { Outlet } from 'react-router-dom'
 import Header from './header/'
+import layout from '../../styles/components/layout/layout.module.css'
 
 export default function Layout() {
   return (
-    <div>
+    <div className={layout.layout}>
       <Header />
-      <Outlet />
+      <main className={layout.content}>
+        <Outlet />
+      </main>
     </div>
   )
 }
